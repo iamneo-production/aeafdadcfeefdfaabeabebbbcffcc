ed9213acc17663710fe708315450dad1bdae0543
Iamneo <techadmin@iamneo.ai>
2023-09-25 13:05:19 +0000

    Backup Commit

---
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -0,0 +1,23 @@
+package com.examly.springapp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringappApplication.class, args);
+
+
+//Write Your Code Here
+
+
+         }
+}
